26979fa13f7ddf46bd6a343fe674c5b8a2b436c0
MohdHaji <mhr9@live.com>
2021-07-11 08:54:28 +0300

    Initial Commit

---
+++ Shared/spotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyApp.swift
+//  Shared
+//
+//  Created by Mohamed Haji on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct spotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
